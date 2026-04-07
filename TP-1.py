156fddcc7facaf72a12baae96b504a920b8e7888
Joao Nogueira <jpedro200564@gmail.com>
2026-04-07 14:47:16 +0100

    Exercicio 4

--- TP-1.py
+++ TP-1.py
@@ -35,3 +35,45 @@ plt.pie(gender, labels=gender.index, autopct='%1.1f%%', colors=['pink', 'skyblue
 plt.title('Distribuição de Género nos USA')
 plt.legend()
 plt.show()
+
+
+# Exercicio 4
+def weekly_study_hours(pais, dispositivo):
+    new_df = df[(df['country'] == pais) & (df['device_type'] == dispositivo)]
+    
+    if new_df.empty:
+        return "Sem dados para comparação"
+    
+    melhor_aluno = new_df.loc[new_df['final_grade'].idxmax()]
+    
+    return melhor_aluno['final_grade'], melhor_aluno['study_hours_weekly']
+    
+
+grade, study_hours = weekly_study_hours("USA", "Tablet")
+print(f"highest grade: {grade}  study hours (weekly): {study_hours}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
